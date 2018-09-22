ba013c349ecfa3df755940379ad9b9df3c479e3c
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-09-22 16:41:51 +0200

    C# project Visual Studio

---
+++ JUSToolkit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JUSToolkit
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
