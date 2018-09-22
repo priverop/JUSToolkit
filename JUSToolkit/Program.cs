b3f6c98736ef9f6760be91600ff3d10d58a06fc3
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-09-22 19:31:47 +0200

    Export BIN to PO

--- JUSToolkit/Program.cs
+++ JUSToolkit/Program.cs
@@ -1,12 +1,52 @@
-﻿using System;
-
-namespace JUSToolkit
+﻿namespace JUSToolkit
 {
+    using System;
+
     class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 3 && args.Length < 2)
+            {
+                Console.Write("Wrong arguments.");
+                showUsage();
+            }
+            else
+            {
+                TextManager tm = new TextManager();
+
+                switch (args[0])
+                {
+                    case "-extractText":
+                        tm.LoadFile(args[1]);
+                        tm.ExportPO();
+                        break;
+
+                    case "-insertText":
+                        tm.LoadFile(args[1]);
+                        //tm.ImportPO(args[2]);
+                        //tm.ExportBin();
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong arguments.");
+                        showUsage();
+                        break;
+                }
+            }
+        }
+
+        private static void showUsage()
+        {
+            Console.WriteLine("Usage: JUSToolkit.exe -extractText <fileToExtract>");
+            Console.WriteLine("Usage: JUSToolkit.exe -insertText <originalFile> <po>");
+        }
+
+        private static void showCredits()
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("== JUS TOOLKIT by Nex ==");
+            Console.WriteLine("=========================");
         }
     }
 }
---
+++ JUSToolkit/TextManager.cs
@@ -0,0 +1,80 @@
+﻿namespace JUSToolkit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using Yarhl.IO;
+    using Yarhl.Media.Text;
+    using Yarhl.FileFormat;
+
+    public class TextManager
+    {
+        public string FileName { get; set; }
+        public List<int> Pointers { get; }
+        public List<string> Text { get; }
+
+        public TextManager()
+        {
+            this.Pointers = new List<int>();
+            this.Text = new List<string>();
+        }
+
+        public void LoadFile(string fileToExtractName)
+        {
+            using (DataStream fileToExtractStream = new DataStream(fileToExtractName, FileOpenMode.Read))
+            {
+                DataReader fileToExtractReader = new DataReader(fileToExtractStream)
+                {
+                    DefaultEncoding = new Yarhl.Media.Text.Encodings.EscapeOutRangeEnconding("ascii")
+                };
+
+                this.FileName = fileToExtractName;
+
+                long currentPosition = fileToExtractStream.Position;
+                int firstPointer = fileToExtractReader.ReadInt32();
+                int secondPointer = fileToExtractReader.ReadInt32();
+                long pointerTableSize = firstPointer - currentPosition;
+                fileToExtractStream.Position = currentPosition;
+
+                // Read all the pointers
+
+                while (fileToExtractStream.Position != firstPointer)
+                {
+                    int pointer = fileToExtractReader.ReadInt32();
+                    this.Pointers.Add(pointer);
+                }
+
+                if(firstPointer > secondPointer){
+                    fileToExtractStream.Position = firstPointer;
+
+                    while(!fileToExtractReader.Stream.EndOfStream){
+                        this.Text.Add(fileToExtractReader.ReadString());
+                    }
+
+                }
+            }
+        }
+
+        public void ExportPO(){
+
+            Po poExport = new Po
+            {
+                Header = new PoHeader("Jump Ultimate Stars", "TranScene", "es")
+                {
+                    LanguageTeam = "TranScene",
+                }
+            };
+
+            for (int i = 0; i < this.Text.Count; i++)
+            {
+                string sentence = this.Text[i];
+                if (string.IsNullOrEmpty(sentence))
+                    sentence = "<!empty>";
+                poExport.Add(new PoEntry(sentence) { Context = i.ToString() });
+            }
+
+            poExport.ConvertTo<BinaryFormat>().Stream.WriteTo(this.FileName + ".po");
+        }
+
+    }
+}
