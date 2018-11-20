38bfcc3a33f7a81bd7e8db16678ea7a9da7b9c86
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-11-20 12:23:45 +0100

    Added Insert/Export argument

--- JUSToolkit/Program.cs
+++ JUSToolkit/Program.cs
@@ -23,7 +23,7 @@
         {
             BasicConfigurator.Configure();
 
-            if (args.Length != 2)
+            if (args.Length < 3 || args.Length > 4)
             {
                 log.Error("Wrong arguments.");
                 showUsage();
@@ -32,8 +32,13 @@
             {
                 showCredits();
 
-                string inputFileName = args[0];
-                string outputPath = args[1];
+                string type = args[0];
+                string inputFileName = args[1];
+                string outputPath = args[2];
+
+                if(args[0] == "-i"){
+                    string fileToInsert = args[3];
+                }
 
                 Identify i = new Identify();
 
@@ -51,59 +56,67 @@
 
                 log.Info("Format detected: " + inputFormat.ToString());
 
-                switch (inputFormat.ToString())
-                {
-                    case FORMATPREFIX + "BinTutorial":
-
-                        n.Transform<BinaryFormat2BinTutorial, BinaryFormat, BinTutorial>()
-                        .Transform<Bin2Po, BinTutorial, Po>()
-                        .Transform<Po2Binary, Po, BinaryFormat>()
-                        .Stream.WriteTo(outputPath + Path.PathSeparator + inputFileName + ".po");
-
-                        break;
-
-                    case FORMATPREFIX + "ALAR3":
-
-                        var folder = n.Transform<BinaryFormat2Alar3, BinaryFormat, ALAR3>()
-                            .Transform<Alar2Nodes, ALAR3, NodeContainerFormat>();
-                            
-                        SaveToDir(folder, outputPath);
-
-                        break;
-
-                    case FORMATPREFIX + "ALAR2":
-
-                        var root = n.Transform<BinaryFormat2Alar2, BinaryFormat, ALAR2>()
-                            .Transform<Alar2Nodes, ALAR3, NodeContainerFormat>();
-
-                        SaveToDir(root, outputPath);
-
-                        break;
-
-                    case FORMATPREFIX + "DIG":
-
-                        DIG dig = n.Transform<Binary2DIG, BinaryFormat, DIG>().GetFormatAs<DIG>();
-
-                        var img = dig.Pixels.CreateBitmap(dig.Palette, 0);
-
-                        img.Save(inputFileName + ".png");
-
-                        break;
-
+                switch(type){
+                    case "-e":
+                        Export(inputFormat.ToString(), n, outputPath, inputFileName);
+                    break;
                 }
-
-                log.Info("Transformado " + n.Name);
-
-                log.Info("Guardada exportación en " + outputPath);
 
                 log.Info("Program completed.");
 
             }
         }
 
+        private static void Export(string format, Node n, string outputPath, string inputFileName){
+            switch (format)
+            {
+                case FORMATPREFIX + "BinTutorial":
+
+                    n.Transform<BinaryFormat2BinTutorial, BinaryFormat, BinTutorial>()
+                    .Transform<Bin2Po, BinTutorial, Po>()
+                    .Transform<Po2Binary, Po, BinaryFormat>()
+                    .Stream.WriteTo(outputPath + Path.PathSeparator + inputFileName + ".po");
+
+                    break;
+
+                case FORMATPREFIX + "ALAR3":
+
+                    var folder = n.Transform<BinaryFormat2Alar3, BinaryFormat, ALAR3>()
+                        .Transform<Alar2Nodes, ALAR3, NodeContainerFormat>();
+
+                    SaveToDir(folder, outputPath);
+
+                    break;
+
+                //case FORMATPREFIX + "ALAR2":
+
+                    //var root = n.Transform<BinaryFormat2Alar2, BinaryFormat, ALAR2>()
+                    //    .Transform<Alar2Nodes, ALAR3, NodeContainerFormat>();
+
+                    //SaveToDir(root, outputPath);
+
+                    //break;
+
+                case FORMATPREFIX + "DIG":
+
+                    DIG dig = n.Transform<Binary2DIG, BinaryFormat, DIG>().GetFormatAs<DIG>();
+
+                    var img = dig.Pixels.CreateBitmap(dig.Palette, 0);
+
+                    img.Save(inputFileName + ".png");
+
+                    break;
+
+            }
+            log.Info("Transformado " + n.Name);
+
+            log.Info("Guardada exportación en " + outputPath);
+        }
+
         private static void showUsage()
         {
-            log.Info("Usage: JUSToolkit.exe <fileToExtract> <dirToExtractTo>");
+            log.Info("Usage: JUSToolkit.exe -e <fileToExtract> <dirToExtractTo>");
+            log.Info("Usage: JUSToolkit.exe -i <originalFile> <dirToExtractTo> <fileToInsert>");
         }
 
         private static void showCredits()
