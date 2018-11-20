80a436c07d2fe57e150a01cc14b3d160cc25af7c
Pablo Rivero <p.riverop@alumnos.urjc.es>
2018-11-20 16:05:16 +0100

    Fixed bug palettes with special case

--- JUSToolkit/Converters/Images/Binary2DIG.cs
+++ JUSToolkit/Converters/Images/Binary2DIG.cs
@@ -20,20 +20,33 @@
             DataReader reader = new DataReader(source.Stream);
             reader.Stream.Position = 0;
 
-            DIG dig = new DIG();
-            reader.ReadBytes(4); // Magic
-            reader.ReadByte(); // Type
-            reader.ReadByte(); // Type_alt
-            reader.ReadByte(); // PaletteNumber
-            reader.ReadByte(); // Unknown BitDepth?
-            reader.ReadInt16(); // Unk
-            reader.ReadInt16(); // Unk
+            DIG dig = new DIG
+            {
+                Magic = reader.ReadBytes(4),
+                Type = reader.ReadByte(),
+                Type_alt = reader.ReadByte(),
+                PaletteNumber = reader.ReadByte(),
+                Unknown = reader.ReadByte(),
+                Unk = reader.ReadUInt16(),
+                Unk2 = reader.ReadUInt16()
+            };
 
             long startPalette = reader.Stream.Position;
 
-            while (reader.ReadInt32() != 0){}
+            int returningBytes = 8;
 
-            long endPalette = reader.Stream.Position - 4;
+            if(reader.ReadInt32() == 0){
+                while (reader.ReadInt32() == 0) { }
+                startPalette = reader.Stream.Position - 4;
+                returningBytes += 4;
+            }
+            else{
+                reader.Stream.Position = startPalette;
+            }
+
+            while (reader.ReadInt64() != 0) { }
+
+            long endPalette = reader.Stream.Position - returningBytes;
 
             long paletteSize = endPalette - startPalette;
 
@@ -49,8 +62,8 @@
 
             long bytesUntilEnd = reader.Stream.Length - reader.Stream.Position;
 
-            ColorFormat format = paletteSize == 64 
-                ? ColorFormat.Indexed_4bpp : ColorFormat.Indexed_8bpp;
+            ColorFormat format = paletteSize == 512 
+                ? ColorFormat.Indexed_8bpp : ColorFormat.Indexed_4bpp;
 
             dig.Pixels.SetData(
                 reader.ReadBytes((int)bytesUntilEnd),
--- JUSToolkit/Formats/DIG.cs
+++ JUSToolkit/Formats/DIG.cs
@@ -9,5 +9,13 @@
         public PixelArray Pixels { get; set; }
 
         public Palette Palette { get; set; }
+
+        public byte[] Magic { get; set; }
+        public byte Type { get; set; }
+        public byte Type_alt { get; set; }
+        public byte PaletteNumber { get; set; }
+        public byte Unknown { get; set; }
+        public ushort Unk { get; set; }
+        public ushort Unk2 { get; set; }
     }
 }
