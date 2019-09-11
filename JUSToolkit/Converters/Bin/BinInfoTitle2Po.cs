39e06d5212198c83c047d7f55f2064d30ad4fef0
Darkc0m <alex011298@gmail.com>
2019-09-11 18:15:53 +0200

    Intial commit

---
+++ JUSToolkit/Converters/Bin/BinInfoTitle2Po.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JUSToolkit.Formats;
+using Yarhl.FileFormat;
+using Yarhl.Media.Text;
+
+namespace JUSToolkit.Converters.Bin
+{
+    class BinInfoTitle2Po : IConverter<BinInfoTitle, Po>
+    {
+        public Po Convert(BinInfoTitle source)
+        {
+            Po exportPo = new Po()
+            {
+                Header = new PoHeader("Jump Ultimate Stars", "TranScene", "es")
+                {
+                    LanguageTeam = "TranScene"
+                }
+            };
+
+            for(int i = 0; i < source.Text.Count; i++)
+            {
+                exportPo.Add(new PoEntry(source.Text.ElementAt(i)) { Context = i.ToString()});
+            }
+
+            return exportPo;
+        }
+    }
+}
---
+++ JUSToolkit/Converters/Bin/Binary2BinInfoTitle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yarhl.FileFormat;
+using JUSToolkit.Formats;
+using Yarhl.IO;
+
+namespace JUSToolkit.Converters.Bin
+{
+    class Binary2BinInfoTitle : IConverter<BinaryFormat, BinInfoTitle>
+    {
+        public BinInfoTitle Convert(BinaryFormat source)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+
+            DataReader reader = new DataReader(source.Stream)
+            {
+                DefaultEncoding = new Yarhl.Media.Text.Encodings.EscapeOutRangeEncoding("ascii")
+            };
+
+            var bin = new BinInfoTitle();
+
+            //Posicionar el reader al comienzo del texto
+            int firstPointer = reader.ReadInt32();
+            reader.Stream.Position = firstPointer;
+
+            //Guardar todo el texto en bin
+            while (!reader.Stream.EndOfStream)
+            {
+                bin.Text.Add(reader.ReadString());
+            }
+
+            return bin;
+        }
+    }
+}
