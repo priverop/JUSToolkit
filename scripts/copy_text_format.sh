85ed42ad3aa1c496f827ff6c9d544ab49fd09cb8
Pablo Rivero <priverop>
2023-01-07 17:06:44 +0100

    fix: add variable replacement to copy script

--- scripts/copy_text_format.sh
+++ scripts/copy_text_format.sh
@@ -12,13 +12,17 @@ cd "/Users//Dev/JUSToolkit/src/JUS.Tool/Texts"
 cd "Formats"
 cp "${OLD_FORMAT}.cs" "${NEW_FORMAT}.cs"
 sed -i "" "s/${OLD_FORMAT}/${NEW_FORMAT}/g" "${NEW_FORMAT}.cs"
+sed -i "" "s/${OLD_FORMAT,}/${NEW_FORMAT,}/g" "${NEW_FORMAT}.cs"
 
 cp "${OLD_FORMAT}Entry.cs" "${NEW_FORMAT}Entry.cs"
 sed -i "" "s/${OLD_FORMAT}/${NEW_FORMAT}/g" "${NEW_FORMAT}Entry.cs"
+sed -i "" "s/${OLD_FORMAT,}/${NEW_FORMAT,}/g" "${NEW_FORMAT}Entry.cs"
 
 cd "../Converters"
 cp "${OLD_FORMAT}2Po.cs" "${NEW_FORMAT}2Po.cs"
 sed -i "" "s/${OLD_FORMAT}/${NEW_FORMAT}/g" "${NEW_FORMAT}2Po.cs"
+sed -i "" "s/${OLD_FORMAT,}/${NEW_FORMAT,}/g" "${NEW_FORMAT}2Po.cs"
 
 cp "Binary2${OLD_FORMAT}.cs" "Binary2${NEW_FORMAT}.cs"
 sed -i "" "s/${OLD_FORMAT}/${NEW_FORMAT}/g" "Binary2${NEW_FORMAT}.cs"
+sed -i "" "s/${OLD_FORMAT,}/${NEW_FORMAT,}/g" "Binary2${NEW_FORMAT}.cs"
