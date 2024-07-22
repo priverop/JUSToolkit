39e0f77b6f3f446441b1887133f343782ab54448
Pablo Rivero <priverop>
2024-07-22 17:11:41 +0200

    refactor(text): encapsulate export jquiz

--- src/JUS.CLI/JUS/CommandLine.cs
+++ src/JUS.CLI/JUS/CommandLine.cs
@@ -193,6 +193,12 @@ namespace JUSToolkit.CLI.JUS
             };
             export.Handler = CommandHandler.Create<string, string>(TextExportCommands.Export);
 
+            var exportJQuiz = new Command("exportJQuiz", "Export JQuiz .bin file to multiple .po files") {
+                new Option<string>("--bin", "the input jquiz.bin file", ArgumentArity.ExactlyOne),
+                new Option<string>("--output", "the output folder", ArgumentArity.ExactlyOne),
+            };
+            exportJQuiz.Handler = CommandHandler.Create<string, string>(TextExportCommands.JQuizExport);
+
             var deckExport = new Command("deckExport", "Export .bin files from a folder, to a single .po file") {
                 new Option<string>("--directory", "the directory with the .bin files", ArgumentArity.ExactlyOne),
                 new Option<bool>("--pdeck", "true for pdeck files", ArgumentArity.ZeroOrOne),
@@ -234,6 +240,7 @@ namespace JUSToolkit.CLI.JUS
             return new Command("texts", "Export or import bin files to Po") {
                 export,
                 deckExport,
+                exportJQuiz,
                 batchExport,
                 import,
                 deckImport,
