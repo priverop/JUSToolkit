ead54d7cabdaf88ed61515a834297eb5b8194b8b
priverop <priverop@github.com>
2025-04-26 21:14:28 +0200

    chore: remove commented code

--- src/JUS.Tool/Graphics/Converters/BinaryKShape2SpriteCollection.cs
+++ src/JUS.Tool/Graphics/Converters/BinaryKShape2SpriteCollection.cs
@@ -85,22 +85,16 @@ namespace JUSToolkit.Graphics.Converters
             var reader = new DataReader(source.Stream);
             var kshape = new KShapeSprites();
 
-            // Console.WriteLine("Index | Position1 | Element | Position2 | NumElements | E | Position3");
-            // Console.WriteLine("------|-----------|---------|-----------|-------------|----|----------");
 
             for (int i = 0; i < NumGroups; i++) {
                 source.Stream.Position = i * 4; // First section
-                long position1 = source.Stream.Position;
                 int firstElement = reader.ReadInt32();
 
                 source.Stream.Position = GroupSizeOffset + (i * 4); // Second section
-                long position2 = source.Stream.Position;
                 int numElements = reader.ReadInt32();
 
                 for (int e = 0; e < numElements; e++) {
                     source.Stream.Position = DataOffset + ((firstElement + e) * EntrySize); // Third section
-                    long position3 = source.Stream.Position;
-                    // Console.WriteLine($"{i,5} | {position1.ToString("X"),9} | {firstElement.ToString("X"),7} | {position2.ToString("X"),9} | {numElements.ToString("X"),11} | {e,2} | {position3.ToString("X"),9}");
                     Sprite sprite = ReadSprite(reader);
                     kshape.AddSprite(i, e, sprite);
                 }
@@ -127,7 +121,7 @@ namespace JUSToolkit.Graphics.Converters
                 // If the segment index is 0, then it's transparent, skip.
                 if (info[i] > 0) {
                     int segmentIdx = info[i] - 1;
-                    int tileIdx = (segmentIdx * TilesPerSegment) + 1; // skip first transparent tile
+                    int tileIdx = segmentIdx * TilesPerSegment; // skip first transparent tile
                     var segment = new ImageSegment {
                         Width = SegmentDimensions,
                         Height = SegmentDimensions,
