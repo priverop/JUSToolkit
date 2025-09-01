f5886ec1102f56c97a91239761ffe16e9ef860cb
priverop <priverop@github.com>
2025-09-01 11:50:27 +0200

    WIP test: trying to debug the error

--- src/JUS.CLI/JUS/Graphics/DtxCommands.cs
+++ src/JUS.CLI/JUS/Graphics/DtxCommands.cs
@@ -137,8 +137,24 @@ namespace JUSToolkit.CLI.JUS
 
             dtx3.Children["image"].ChangeFormat(updatedImage);
 
-            new Dtx3ToBinary().Convert(dtx3.GetFormatAs<NodeContainerFormat>())
-                .Stream.WriteTo(Path.Combine(output, Path.GetFileName(dtx)));
+            var spriteParams2 = new Sprite2IndexedImageParams {
+                RelativeCoordinates = SpriteRelativeCoordinatesKind.Center,
+                FullImage = updatedImage,
+            };
+            var indexedImageParams = new IndexedImageBitmapParams {
+                Palettes = updatedImage,
+            };
+            for (int i = 0; i < dtx3.Children["sprites"].Children.Count; i++) {
+                var spriteNode = dtx3.Children["sprites"].Children[i];
+                // Cloning the node so we can transform it
+                var pngNode = new Node(spriteNode.Name, spriteNode.GetFormatAs<Sprite>())
+                            .TransformWith(new Sprite2IndexedImage(spriteParams2))
+                            .TransformWith(new IndexedImage2Bitmap(indexedImageParams));
+                pngNode.Stream.WriteTo(Path.Combine(output, $"zzz/{pngNode.Name}.png"));
+            }
+
+            // new Dtx3ToBinary().Convert(dtx3.GetFormatAs<NodeContainerFormat>())
+            //     .Stream.WriteTo(Path.Combine(output, Path.GetFileName(dtx)));
 
             Console.WriteLine("Done!");
         }
