5f883b67d8837eebb732c44ccbeb9f6e8d28317d
priverop <priverop@github.com>
2025-09-01 23:49:26 +0200

    chore: rename the command, no tx

--- src/JUS.CLI/JUS/Graphics/DtxCommands.cs
+++ src/JUS.CLI/JUS/Graphics/DtxCommands.cs
@@ -72,14 +72,13 @@ namespace JUSToolkit.CLI.JUS
         /// <param name="dtx">The .dtx file.</param>
         /// <param name="output">The output folder.</param>
         /// <exception cref="FormatException"><paramref name="dtx"/> file doesn't have a valid format.</exception>
-        public static void ExportDtx3TxImage(string dtx, string output)
+        public static void ExportDtx3Image(string dtx, string output)
         {
-            Console.WriteLine("Exporting Dtx3Tx Image");
+            Console.WriteLine("Exporting Dtx3 Image");
             Console.WriteLine("DTX: " + dtx);
 
             PathValidator.ValidateFile(dtx);
 
-            // Sprites + pixels + palette
             using Node dtx3 = NodeFactory.FromFile(dtx, FileOpenMode.Read)
                 .TransformWith<LzssDecompression>()
                 .TransformWith<BinaryToDtx3>();
