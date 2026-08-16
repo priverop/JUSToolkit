1d3025a887c4f30ac42b3c67cfccd8588f6f780e
priverop <priverop@users.noreply.github.com>
2026-08-16 13:01:09 +0200

    test(deck): add missing deck test

--- src/JUS.Tests/Texts/DeckFormatTest.cs
+++ src/JUS.Tests/Texts/DeckFormatTest.cs
@@ -1,13 +1,17 @@
 ﻿using System;
+using JUS.Tool.Containers;
+using JUS.Tool.Containers.Converters;
 using JUS.Tool.Texts.Converters;
 using JUS.Tool.Texts.Formats;
 using NUnit.Framework;
+using SceneGate.Ekona.Containers.Rom;
 using Yarhl.FileSystem;
 using Yarhl.IO;
 using Yarhl.Media.Text;
 
 namespace JUS.Tests.Texts
 {
+    [TestFixture]
     public class DeckFormatTest
     {
         private string resPath = string.Empty;
@@ -19,6 +23,57 @@ namespace JUS.Tests.Texts
             resPath = Path.GetFullPath(programDir + "/../../../Resources/Texts/Deck/");
 
             Assert.That(Directory.Exists(resPath), Is.True, "The resources folder does not exist");
+        }
+
+        private static readonly Lazy<NitroRom> Root = new(TestDataBase.ReadSoftware);
+
+        private static Alar? DeckAlar;
+
+        public static IEnumerable<TestCaseData> GetDeckBinPaths()
+        {
+            if (!File.Exists(TestDataBase.SoftwareNitroRomPath)) {
+                return [];
+            }
+
+            Node deckAar = Navigator.SearchNode(Root.Value.Data, "deck/Deck.aar") ?? throw new ArgumentException("Deck.aar not found");
+            DeckAlar = deckAar.TransformWith<Binary2Alar>().GetFormatAs<Alar>()!;
+
+            return Navigator.IterateNodes(DeckAlar.Root, NavigationMode.DepthFirst)
+                .Where(n => !n.IsContainer && n.Name.EndsWith(".bin") && !n.Name.StartsWith('p'))
+                .Select(n => new TestCaseData(n.Path));
+        }
+
+        [TestCaseSource(nameof(GetDeckBinPaths))]
+        public void TwoWaysIdenticalStreams(string binPath)
+        {
+            Assert.Ignore("The new Deck limit is too short, 9/300 tests fail. This will be removed as soon as we hack the deck limits.");
+            TestDataBase.IgnoreIfFileDoesNotExist(TestDataBase.SoftwareNitroRomPath);
+
+            IBinary? original = Navigator.SearchNode(DeckAlar!.Root, binPath)?.GetFormatAs<IBinary>();
+            Assert.That(original, Is.Not.Null);
+
+            AssertTwoWaysIdentical(original);
+        }
+
+        private static void AssertTwoWaysIdentical(IBinary original)
+        {
+            // Debo comprobar que no haya errores en los pasos intermedios? try catch?
+            Deck originalDeck = new Binary2Deck().Convert(original);
+            var originalContainer = new NodeContainerFormat();
+            originalContainer.Root.Add(new Node("test", originalDeck));
+            Po deckPo = new Deck2Po().Convert(originalContainer);
+            NodeContainerFormat newDeckContainer = new Deck2Po().Convert(deckPo);
+            Deck newDeck = newDeckContainer.Root.Children[0].GetFormatAs<Deck>()!;
+            BinaryFormat newBinary = new Binary2Deck().Convert(newDeck);
+            Assert.That(newBinary, Is.Not.Null);
+
+            byte[] originalData = new byte[(int)original.Stream.Length];
+            original.Stream.Position = 0;
+            original.Stream.ReadExactly(originalData);
+            byte[] newData = new byte[(int)newBinary.Stream.Length];
+            newBinary.Stream.Position = 0;
+            newBinary.Stream.ReadExactly(newData);
+            Assert.That(newData, Is.EqualTo(originalData));
         }
 
         [Test]
